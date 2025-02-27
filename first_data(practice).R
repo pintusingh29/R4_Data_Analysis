5a49a08147bc3f4dee68cf413fe773596075e167
pintukumarsingh29 <pintusingh68902@gmail.com>
2025-02-27 23:39:38 +0000

    ggplot2

--- first_data(practice).R
+++ first_data(practice).R
@@ -1,2 +1,32 @@
-data("airquality")
-View(airquality)
+library(tidyverse)
+View(diamonds)
+head(diamonds)
+
+str(diamonds)
+
+glimpse(diamonds)
+
+colnames(diamonds)
+
+rename(diamonds, carat_new = carat)
+
+rename(diamonds, carat_new = carat, cut_new = cut)
+
+summarize(diamonds, mean_carat = mean(carat))
+
+ggplot(data = diamonds, aes(x = carat, y = price)) + geom_point()
+
+ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) + geom_point()
+
+ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) + geom_point() + facet_wrap(~cut)
+
+
+
+
+
+
+
+
+
+
+
